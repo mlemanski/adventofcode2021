81e3fcd9034317449be265f229140fce77607d71
Matthew Lemanski <matthew.lemanski@gmail.com>
2021-12-10 21:28:35 -0500

    day 6

---
+++ 06/solution1.py
@@ -0,0 +1,22 @@
+
+import common
+
+INPUT = 'input.txt'
+# INPUT = 'sample.txt'
+
+with open(INPUT) as f:
+    LINE = f.readline().rstrip()
+
+ages = [int(_) for _ in LINE.split(',')]
+
+for generation in range(0, 80):
+    num_new_fish = 0
+    for i, age in enumerate(ages):
+        if age == 0:
+            num_new_fish += 1
+            ages[i] = 6
+        else:
+            ages[i] -= 1
+    ages.extend([8]*num_new_fish)
+
+print(len(ages))
